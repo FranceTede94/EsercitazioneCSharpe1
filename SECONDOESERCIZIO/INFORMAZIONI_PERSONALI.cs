8176b9103c82cbfacde116ac97eed4f9fbce7ebb
FranceTede94 <francesco.tedesco94@hotlook.com>
2025-03-25 23:07:37 +0100

    Esercitazioni C#

---
+++ SECONDOESERCIZIO/INFORMAZIONI_PERSONALI.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SECONDOESERCIZIO
+{
+    internal class INFORMAZIONI_PERSONALI
+    {
+        // Proprietà pubbliche
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Codicefiscale { get; set; }
+        public string Indirizzo { get; set; }
+        public string Luogonascita { get; set; }
+
+        // Costruttore
+        public INFORMAZIONI_PERSONALI(string nome, string cognome, string codicefiscale, string indirizzo, string luogonascita)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Codicefiscale = codicefiscale;
+            Indirizzo = indirizzo;
+            Luogonascita = luogonascita;
+        }
+
+        public string NomeCompleto()
+        {
+            return $"{Nome} {Cognome}";
+        }
+
+        public string IndirizzoFormattato()
+        {
+            return $"Via {Indirizzo}, {Luogonascita}";
+        }
+
+        public bool VerificaCodiceFiscale()
+        {
+            return Codicefiscale.Length == 16;
+        }
+
+        public string StampaInformazioni()
+        {
+            return $"Mi chiamo {Nome} {Cognome}. Il mio codice fiscale è {Codicefiscale}. Vivo in via {Indirizzo} e sono nato a {Luogonascita}.";
+        }
+
+        public string InserisciNome(string nome, string cognome)
+        {
+            return $"{nome} {cognome}";
+        }
+    }
+}
---
+++ SECONDOESERCIZIO/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SECONDOESERCIZIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            INFORMAZIONI_PERSONALI info = new INFORMAZIONI_PERSONALI
+               (
+               "Francesco", "Giaffreda", "FGBFNC87E05B945J", "Via Napoli, 55", "Gallipoli"
+               );
+
+            Console.WriteLine(info.Luogonascita);
+
+            info.Luogonascita = "Casarano";
+
+            Console.WriteLine(info.Luogonascita);
+
+            Console.WriteLine(info.NomeCompleto());
+
+            Console.WriteLine(info.IndirizzoFormattato());
+
+            Console.WriteLine(info.VerificaCodiceFiscale());
+
+            Console.WriteLine(info.StampaInformazioni());
+
+            info.InserisciNome("Francesco", "Greco");
+            Console.WriteLine(info.InserisciNome("Francesco", "Greco"));
+
+        }
+    }
+}
